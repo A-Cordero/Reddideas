0b1a46d23bb4936c14c60a3ee6830773269e364a
A-Cordero <anthony.cordero.g@uni.pe>
2021-11-14 18:39:30 -0500

    version para Aristicomp

--- app/src/main/java/com/aridev/cordero/twitdeas/core/Constants.kt
+++ app/src/main/java/com/aridev/cordero/twitdeas/core/Constants.kt
@@ -11,5 +11,5 @@ object Constants {
 var language = "EN"
 var style = "dark" // dark
 enum class Tags(val url : String){
-    APPS("https://www.reddit.com/r/AskReddit/comments/85j3ki/reddit_whats_your_million_dollar_app_idea_that/.json?sort=new&limit=10&after="),
+    APPS("https://www.reddit.com/r/AskReddit/comments/85j3ki/reddit_whats_your_million_dollar_app_idea_that/.json?sort=new&limit=30&after="),
 }
--- app/src/main/java/com/aridev/cordero/twitdeas/core/extensions/ViewExtensions.kt
+++ app/src/main/java/com/aridev/cordero/twitdeas/core/extensions/ViewExtensions.kt
@@ -4,21 +4,10 @@ import android.os.Handler
 import android.os.Looper
 import android.view.View
 import android.view.animation.AnimationUtils
-import android.widget.ImageView
-import androidx.cardview.widget.CardView
 import com.aridev.cordero.twitdeas.R
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 
-fun ImageView.loadUrlImage(url:String){
-    Glide.with(context)
-        .load(url)
-        .transition(DrawableTransitionOptions.withCrossFade())
-        .fitCenter()
-        .into(this)
-}
 
- fun View.getCenterView(): Float{
+fun View.getCenterView(): Float{
      val location = IntArray(2)
      this.getLocationOnScreen(location)
      val posX = location[0]
